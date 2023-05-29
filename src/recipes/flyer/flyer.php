c0e826e0c05b60c4e02c1e79c49bd92696922ce2
Bagas Wahyu Hidayah <devops@botika.online>
2023-05-29 11:44:50 +0700

    namespace fixes

--- src/recipes/flyer/flyer.php
+++ src/recipes/flyer/flyer.php
@@ -7,11 +7,4 @@ require __DIR__ . '/../../../vendor/autoload.php';
 /**
  * Zipped artifact will contain... the artifact directory. Directory may have flyer.toml or flyer.php file to configure the deployment.
  * */
-task('deploy:prepare', function () {
-
-})->desc(<<<EOF
-deploy:prepare:
-    1. Zipped artifac
-    Extract zipped artifact to a destined `{{deploy_path}}`
-    2. 
-EOF);
+\Deployer\task('deploy:prepare', function () {});
