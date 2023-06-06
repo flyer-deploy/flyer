e8deea6125cfe039298914f9ce0c8df05556eae5
Perdana <perdanaarta.botika@gmail.com>
2023-06-06 17:01:42 +0700

    dir

--- src/recipes/flyer.php
+++ src/recipes/flyer.php
@@ -154,13 +154,15 @@ task('deploy:shared_dir', function () {
     $project_name = get('project_name');
     $repo_name = get('repo_name');
 
-    mkdir_if_not_exists("/var/share/$project_name/$repo_name");
+    foreach(get('shared_dir') as $dir) {
+        mkdir_if_not_exists("/var/share/$project_name/$repo_name/$dir");
 
-    writeln("Moving shared dir content.");
-    run("mv -f {{shared_dir}} /var/share/$project_name/$repo_name");
-
-    writeln("Creating shared dir symlink.");
-    run("ln -sfn /var/share/$project_name/$repo_name {{shared_dir}}");
+        writeln("Moving shared dir content.");
+        run("mv -f {{new_release_path}}/$dir /var/share/$project_name/$repo_name/$dir");
+    
+        writeln("Creating shared dir symlink.");
+        run("ln -sfn /var/share/$project_name/$repo_name/$dir {{new_release_path}}/$dir");
+    }
 });
 
 task('deploy', function () {
