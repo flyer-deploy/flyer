ef700f8897bb9e2106a8def7a7b02c623c416973
Bagas Wahyu Hidayah <devops@botika.online>
2023-06-28 10:19:23 +0700

    cleanup comments and unused `use`

--- src/recipes/deploy/cleanup.php
+++ src/recipes/deploy/cleanup.php
@@ -2,14 +2,11 @@
 
 namespace Deployer;
 
-use React\EventLoop\Factory;
-use React\Promise\Deferred;
-
 task('deploy:cleanup', function () {
     $release_list = get('release_list');
     $release_name = get('release_name');
 
-    $delete_queue = array_filter($release_list, fn ($release) => $release !== $release_name);
+    $delete_queue = array_filter($release_list, fn($release) => $release !== $release_name);
     foreach ($delete_queue as $release) {
         if (get('async_cleanup') == 1) {
             run("log_file=/tmp/{{app_id}}.{{release_name}}.log");
@@ -18,21 +15,4 @@ task('deploy:cleanup', function () {
             run("rm -rf {{deploy_path}}/$release");
         }
     }
-
-    // $loop = Factory::create();
-
-    // $functions = array_map(function($release) {
-    //     return function() use ($release) {
-    //         run("rm -rf {{deploy_path}}/$release");
-    //     };
-    // }, $delete_queue);
-
-    // foreach ($functions as $function) {
-    //     $loop->addTimer(0, function () use ($function) {
-    //         // Execute the function asynchronously
-    //         $function();
-    //     });
-    // }
-    
-    // $loop->run(); // Start the event loop
 });
